383fca2dbdaee9843f9f21e8e86174dadf08b667
pkr187 <pankajroman187@gmail.com>
2022-11-16 00:11:21 +0530

    [pankaj] Add:EmployeePayRollADO

---
+++ EmployeePayRollADO.Net/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace EmployeePayRollADO.Net
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to employee payroll");
+            EmployeeModel employee = new EmployeeModel();
+            EmployeeRepo repo = new EmployeeRepo();
+            employee.EmployeeName = "Suraj";
+            employee.Phoneno = "7558570105";
+            employee.Address = "Karve Nagar";
+            employee.Department = "Hr";
+            employee.Gender = 'M';
+            employee.BasicPay = 10000;
+            employee.Deduction = 1000;
+            employee.TaxablePay = 2000;
+            employee.Tax = 111;
+            employee.NetPay = 1111;
+            employee.StartDate = DateTime.Now;
+            employee.City = "Pune";
+            employee.Country = "India";
+
+            //repo.AddEmployee(employee);
+            repo.GetAllEmployee();
+
+        }
+    }
+}
